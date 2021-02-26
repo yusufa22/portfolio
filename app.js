27c9854ea0af2fe0f556c8fa4e102350dba5e8fc
Ayub Yusuf <ayubabdikarim@protonmail.com>
2021-02-26 02:24:11 +0000

    commited

--- app.js
+++ app.js
@@ -75,4 +75,4 @@ for(i=0; i<document.getElementsByClassName('projectsmenuitems').length; i++){
     })
 }
 
-setTimeout(()=>{console.log(document.querySelector('.g-recaptcha').children[0].style.margin = 'auto')}, 2000)
+setTimeout(()=>{document.querySelector('.g-recaptcha').children[0].style.margin = 'auto'}, 2000)
